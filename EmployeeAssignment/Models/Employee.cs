c0fc2b755dee52a05ac4b995d366aebaef9b7f74
ChiragKishorKapadiya <ChiragKishorKapadiya@GEDU-LP333>
2024-01-19 13:12:28 +0530

    Add project files.

---
+++ EmployeeAssignment/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeAssignment.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public double Salary { get; set; }
+        public string Department { get; set; }
+
+    }
+}
